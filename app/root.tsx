5d1d0d860315a25d298019f441b2b93838e48214
Juan Alonso-Allende <juan@aallende.com>
2025-12-26 18:54:15 +0100

    analytics

--- app/root.tsx
+++ app/root.tsx
@@ -10,6 +10,7 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 import ico from "~/assets/juan-2.png";
+import { Analytics } from "@vercel/analytics/react"
 
 export const links: Route.LinksFunction = () => [
   { rel: "icon", type: "image/png", href: ico },
@@ -54,6 +55,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         {children}
         <ScrollRestoration />
         <Scripts />
+        <Analytics />
       </body>
     </html>
   );
